cd3cae6fb9d9e4814358abb08608a4d6e614a7eb
VuManhHung <101306469+h3nesy@users.noreply.github.com>
2024-11-11 00:32:33 +0700

    script.js

--- script.js
+++ script.js
@@ -22,29 +22,17 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/logo.png',
+        name : 'Còn Gì Đau Hơn Chữ Đã Từng',
+        artist : 'hdung x truongnguyen',
+        music : 'images/cgdhcdt.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        img : 'images/logo.png',
+        name : 'Đừng Hỏi Về Anh',
+        artist : 'hdung x truongnguyen',
+        music : 'images/dhva.mp3'
     },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
 ];
 
 loadTrack(track_index);
